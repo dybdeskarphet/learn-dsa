f9c6c2005e56d6a0e9eb9c44167698a1d7843260
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-05-23 20:21:15 +0300

    Add Big O rule about different inptus

---
+++ src/05.big-o-rules.js
@@ -0,0 +1,18 @@
+const firstGroup = [1, 2, 3, 4, 5];
+const secondGroup = [6, 7, 8, 9, 10];
+
+const logTwoDifferentGroupOfBoxes = (boxes, boxes2) => {
+  boxes.forEach((e) => {
+    console.log(e);
+  });
+
+  boxes2.forEach((e) => {
+    console.log(e);
+  });
+};
+
+logTwoDifferentGroupOfBoxes(firstGroup, secondGroup);
+
+/* O(n + m) --> Because it has two different group of arrays. 
+It's not the same number of items that we're going to iterate.
+*/
