807cd3a9e42ddfe7d88d6c06d468352fa9c450cc
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-05-23 19:23:57 +0300

    Add note to the exercise

--- src/04.bigo-exercise-2.js
+++ src/04.bigo-exercise-2.js
@@ -2,12 +2,14 @@ const anotherFunChallenge = (input) => {
   let a = 5; // O(1)
   let b = 10; // O(1)
   let c = 50; // O(1)
+
   for (let i = 0; i < input; i++) {
     // O(n)
     let x = i + 1; // O(n)
     let y = i + 2; // O(n)
     let z = i + 3; // O(n)
   }
+
   for (let j = 0; j < input; j++) {
     // O(n)
     let p = j * 2; // O(n)
@@ -16,3 +18,11 @@ const anotherFunChallenge = (input) => {
   let whoAmI = "I don't know"; // O(1)
 };
 // Big O = 4 + 7n --> O(n)
+
+/* There are 4 rules:
+1. Worst case
+2. Remove constants
+3. Different terms of inputs
+4. Drop non-dominants
+*/
+
