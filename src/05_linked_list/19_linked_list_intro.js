456ec2310fb6a07120749be9a8c236cd37072720
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-06-15 03:50:12 +0300

    Implement insert and remove functions to the linked list

--- src/05_linked_list/19_linked_list_intro.js
+++ src/05_linked_list/19_linked_list_intro.js
@@ -12,42 +12,109 @@ let myLinkedList = {
 }
 */
 
+class Node {
+  constructor(value) {
+    (this.value = value), (this.next = null);
+  }
+}
+
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
+    this.head = newNode;
 
     this.tail = this.head;
     this.length = 1;
   }
 
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
+  }
+
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
 
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
-    return this;
+    return this.printList();
   }
 
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: this.head,
-    };
+    const newNode = new Node(value);
+    newNode.next = this.head;
 
     this.head = newNode;
     this.length++;
-    return this;
+    return this.printList();
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    } else if (index >= this.length) {
+      this.append(value);
+      return this.printList();
+    }
+
+    let currentNodeIndex = 0;
+    let currentNode = this.head;
+
+    while (currentNodeIndex < index - 1) {
+      currentNode = currentNode.next;
+      currentNodeIndex++;
+    }
+
+    const newNode = new Node(value);
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+
+    this.length++;
+    return this.printList();
+  }
+
+  remove(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+      this.length--;
+      if (this.length === 0) {
+        this.tail = null;
+      }
+      return this.printList();
+    }
+
+    let previousNodeIndex = 0;
+    let previousNode = this.head;
+
+    while (previousNodeIndex < index - 1) {
+      previousNode = previousNode.next;
+      previousNodeIndex++;
+    }
+
+    const nodeToRemove = previousNode.next;
+    previousNode.next = nodeToRemove.next;
+
+    if (index === this.length - 1) {
+      this.tail = previousNode;
+    }
+
+    this.length--;
+    return this.printList();
   }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.prepend(1);
 myLinkedList.append(4);
-console.log(myLinkedList);
+console.log(myLinkedList.insert(2, 99));
+console.log(myLinkedList.remove(1));
+// console.log(myLinkedList);
