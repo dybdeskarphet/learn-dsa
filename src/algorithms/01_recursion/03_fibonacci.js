20d37531a7f1b48acdec086384b3b23f12e39a29
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-07-11 22:23:13 +0300

    Add time complexity comments to fibonacci finder

--- src/algorithms/01_recursion/03_fibonacci.js
+++ src/algorithms/01_recursion/03_fibonacci.js
@@ -1,13 +1,16 @@
 // I like this method more
 const fibonacciIterative = (number) => {
+  // O(n)
   let arr = [0, 1];
 
   for (let i = 2; i <= number; i++) {
+    /*
     console.log(
       `i: ${i}\narr[${i - 2}] + arr[${i - 1}] = ${
         arr[i - 2] + arr[i - 1]
       } \narr = [${arr}]\n---`
     );
+    */
     arr.push(arr[i - 2] + arr[i - 1]);
   }
 
@@ -15,11 +18,12 @@ const fibonacciIterative = (number) => {
 };
 
 const fibonacciRecursive = (number) => {
+  // O(2^n)
   if (number < 2) {
     return number;
   }
   return fibonacciRecursive(number - 1) + fibonacciRecursive(number - 2);
 };
 
-// console.log(fibonacciRecursive(8));
-console.log(fibonacciIterative(7));
+// console.log(fibonacciRecursive(40));
+console.log(fibonacciIterative(100));
