cca3006492c0747861417b949cda84aa80ae292e
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-07-15 20:41:19 +0300

    Implement bubble sort

---
+++ src/algorithms/02_sorting/06_bubble_sort.js
@@ -0,0 +1,16 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+const bubbleSort = (array) => {
+  const length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j + 1], array[j]] = [array[j], array[j + 1]];
+      }
+    }
+  }
+};
+
+bubbleSort(numbers); // O(n^2)
+console.log(numbers);
